860ddf958476753db8c741f6046958c106b3c960
Davi-SM <davimoraes@tokenlab.com.br>
2023-10-20 15:34:22 -0300

    Adds implementation to AI ➕

--- back-scraping-node/index.js
+++ back-scraping-node/index.js
@@ -3,23 +3,57 @@ const puppeteer = require("puppeteer");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.get("/", async (req, res) => {
-    //TODO: 
-    await scrapper();
+    //TODO:
+    console.log(req.body);
+    // await scrapper(req.body.data['keys']);
+    await scrapper(req.body.keys);
     res.send("test");
 });
 
-app.listen(port, () => {
-    console.log("running on http://localhost:3000");
+app.listen(port, '26.66.209.67', () => {
+    console.log("running on http://26.66.209.67:3000");
 });
 
-async function scrapper() {
-    const browser = await puppeteer.launch({
-        headless: false,
-    });
+async function scrapper(searchString) {
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     console.log("line 1");
+
+    page.on("download", (download) => {
+        console.log(`Download started: ${download}`);
+
+        // Create a Promise to track the download completion
+        const downloadCompletePromise = new Promise(async (resolve, reject) => {
+            try {
+                // Wait for the 'response' event associated with the download URL
+                const response = await page.waitForResponse(
+                    (response) => response.url() === download.url()
+                );
+
+                // Save the downloaded file
+                const filename = download;
+                const buffer = await response.buffer();
+                fs.writeFileSync(filename, buffer);
+
+                console.log(`Download completed: ${filename}`);
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        });
+
+        // Wait for the download completion
+        downloadCompletePromise
+            .then(() => {
+                console.log("Download completed successfully.");
+            })
+            .catch((error) => {
+                console.error("Download failed:", error);
+            });
+    });
 
     await page.goto(
         "https://www.unitins.br/portaltransparencia/folha-de-pagamento"
@@ -45,7 +79,7 @@ async function scrapper() {
     await page.select("#TipoPesquisa", "Nome");
 
     await page.$("#busca");
-    await page.type("#busca", "Fredson");
+    await page.type("#busca", `${searchString}`);
 
     const searchButton = await page.$("button.btn.btn-default");
 
@@ -57,16 +91,33 @@ async function scrapper() {
 
     console.log("finded the fredson");
 
-    const csvButton = await page.$("a.dt-button.buttons-csv.buttons-html5");
+    await page.waitForNavigation({ waitUntil: "domcontentloaded" });
 
-    if (csvButton) {
-        //is downloading on C:/Users/<username>/Downloads/Demonstrativo de pagamento (1).csv
-        await csvButton.click();
-    } else {
-        console.log("cannot find results");
-    }
+    await page.waitForSelector("#example");
 
-    console.log();
+    // Extract data from the table
+    const data = await page.evaluate(() => {
+        const rows = Array.from(document.querySelectorAll("#example tbody tr"));
+
+        return rows.map((row) => {
+            const columns = row.querySelectorAll("td");
+            return {
+                matricula: columns[0].textContent.trim(),
+                nomeCompleto: columns[1].textContent.trim(),
+                funcao: columns[2].textContent.trim(),
+                lotacaoAtual: columns[3].textContent.trim(),
+                vinculo: columns[4].textContent.trim(),
+                totalVencimento: columns[5].textContent.trim(),
+                descontoIRRF: columns[6].textContent.trim(),
+                descontoPrevidencia: columns[7].textContent.trim(),
+                descontosDiversos: columns[8].textContent.trim(),
+                salarioLiquido: columns[9].textContent.trim(),
+                referencia: columns[10].textContent.trim(),
+            };
+        });
+    });
+
+    console.log(data);
 
     await browser.close();
 }
